12c2e8cb7c44ebb43de11fe35a0ea2e8c3c9fefc
seojonghak <unqocn@gmail.com>
2021-10-31 23:48:05 +0900

    fix: 테스트 폴더구조 정리

---
+++ test/constants/assets.js
@@ -0,0 +1,7 @@
+import JPG_STYLE from "../test/assets/jpg/jpg_style.png";
+
+const JPG_ASSETS = {
+  JPG_STYLE
+};
+
+export default JPG_ASSETS;
---
+++ test/constants/assets/assets.js
@@ -0,0 +1,9 @@
+import SAMPLE from "../../assets/png/sample.png";
+import SAMPLE12 from "../../test/assets/png/sample12.png";
+
+const PNG_ASSETS = {
+  SAMPLE,
+  SAMPLE12
+};
+
+export default PNG_ASSETS;
